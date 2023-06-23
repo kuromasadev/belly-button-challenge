6a2adeba1a8c4b4ea1c9152c36dc8514cad57809
kuromasadev <kuromasadev@gmail.com>
2023-06-22 19:30:10 -0500

    50% Deliverable WORKING
    Stable working version of the file, selecting metadata.id works now

--- static/js/app.js
+++ static/js/app.js
@@ -1,141 +1,129 @@
 // Select the element to display the test subject ID
 const select = d3.select("#selDataset");
 
-// Function to load the JSON data
+// Declare samples and metadata variables
+let samples = [];
+let metadata = [];
+
+// Function to load the JSON data and store information into variables
 function loadJSON() {
   d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
     .then(function(data) {
-      // OTU CHARTS 
-      const samples = data.samples;
-      const selectedIndividual = samples[0];
+      samples = data.samples;
+      metadata = data.metadata;
+     
+      createBarChart();
+      createBubbleChart();
+      createVariables();
+      addMetadataListener();
 
-      const metadata = data.metadata;
-      const selectedMetadata = metadata[0];
-
-      // OUT variables
-      var id = selectedMetadata.id;
-      var ethnicity = selectedMetadata.ethnicity;
-      var gender = selectedMetadata.gender;
-      var age = selectedMetadata.age;
-      var location = selectedMetadata.location;
-      var bbtype = selectedMetadata.bbtype;
-      var wfreq = selectedMetadata.wfreq;
-    
-
-      // Select the element to display
-      const metadataContainer = d3.select("#sample-metadata");
-
-      // Clear any existing metadata
-      metadataContainer.html("");
-        // iterate over key-value pairs
-        Object.entries(selectedMetadata).forEach(([key, value]) => {
-          metadataContainer
-              .append("p")
-              .text(`${key}: ${value}`);
-      });
-
-
-      // CHART 1
-      // Create bar chart
-
-      const barData = [{
-        type: 'bar',
-        x: selectedIndividual.sample_values.slice(0, 10).reverse(),
-        y: selectedIndividual.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
-        text: selectedIndividual.otu_labels.slice(0, 10).reverse(),
-        orientation: 'h'
-      }];
-
-      const barLayout = {
-        title: 'Top 10 OTUs',
-        xaxis: { title: 'Sample Values' },
-        yaxis: { title: 'OTU IDs' }
-      };
-
-      Plotly.newPlot('bar', barData, barLayout);
-    
-      // Update the dropdown menu with the available options
-      select.selectAll("option")
-        .data(samples)
-        .enter()
-        .append("option")
-        .text(sample => sample.id)
-        .property("value", sample => sample.id);
-
-
-      // Create the bubble chart layout
-      const bubbleData = [{
-        x: selectedIndividual.otu_ids,
-        y: selectedIndividual.sample_values,
-        text: selectedIndividual.otu_labels,
-        mode: 'markers',
-        marker: {
-          size: selectedIndividual.sample_values,
-          color: selectedIndividual.otu_ids
-        }
-      }];      
-
-      // Plot the bubble chart using Plotly
-      Plotly.newPlot('bubble', bubbleData, bubbleLayout);
-   
-
-
-        
-      console.log(data); 
-    })  
-    
+      console.log(data);
+    })
     .catch(function(error) {
       // Error handling if data loading fails
       console.log("Error loading JSON data:", error);
     });
 }
-// Event listener for the dropdown menu
-select.on("change", function() {
-  const selectedValue = this.value;
 
-  // Find the selected individual in the samples data
-  const selectedIndividual = samples.find(sample => sample.id === selectedValue);
+// function to initially load the metadata
+function createVariables() {
+  // dropdown
+  select.selectAll("option")
+    .data(metadata)
+    .enter()
+    .append("option")
+    .text(d => d.id);
+}
 
-  // Find the selected individual's metadata
-  const selectedMetadata = metadata.find(meta => meta.id === +selectedValue);
+// Function to create the bar chart
+function createBarChart() {
+  const selectedIndividual = samples[0];
 
-  // Now you can access the selected individual's metadata fields
-  const id = selectedIndividual.id;
-  const ethnicity = selectedIndividual.ethnicity;
-  const gender = selectedIndividual.gender;
-  const age = selectedIndividual.age;
-  const location = selectedIndividual.location;
-  const bbtype = selectedIndividual.bbtype;
-  const wfreq = selectedIndividual.wfreq;
+  const barData = [{
+    type: 'bar',
+    x: selectedIndividual.sample_values.slice(0, 10).reverse(),
+    y: selectedIndividual.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
+    text: selectedIndividual.otu_labels.slice(0, 10).reverse(),
+    orientation: 'h'
+  }];
 
-  document.getElementById("sample-metadata").innerHTML = `
-  <p>ID: ${id}</p>
-  <p>Ethnicity: ${ethnicity}</p>
-  <p>Gender: ${gender}</p>
-  <p>Age: ${age}</p>
-  <p>Location: ${location}</p>
-  <p>BBType: ${bbtype}</p>
-  <p>WFreq: ${wfreq}</p>
-`;
+  const barLayout = {
+    title: 'Top 10 OTUs',
+    xaxis: { title: 'Sample Values' },
+    yaxis: { title: 'OTU IDs' }
+  };
 
-  // Update the bar chart data
+  Plotly.newPlot('bar', barData, barLayout);
+}
+
+// Function to create the bubble chart
+function createBubbleChart() {
+  const selectedIndividual = samples[0];
+
+  const bubbleData = [{
+    x: selectedIndividual.otu_ids,
+    y: selectedIndividual.sample_values,
+    text: selectedIndividual.otu_labels,
+    mode: 'markers',
+    marker: {
+      size: selectedIndividual.sample_values,
+      color: selectedIndividual.otu_ids
+    }
+  }];
+
+  const bubbleLayout = {
+    title: 'Sample Values vs. OTU IDs',
+    xaxis: { title: 'OTU IDs' },
+    yaxis: { title: 'Sample Values' }
+  };
+
+  Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+}
+
+// Function to listen for changes to the metadata_id
+function addMetadataListener() {
+  select.on("change", function() {
+    const selectedValue = d3.select(this).property('value');
+    const selectedIndividual = samples.find(sample => sample.id === selectedValue);
+    const selectedMetadata = metadata.find(meta => meta.id === +selectedValue);
+
+    updateMetadata(selectedMetadata);
+    updateBarChart(selectedIndividual);
+    updateBubbleChart(selectedIndividual);
+  });
+}
+
+// Function to update the metadata display
+function updateMetadata(selectedMetadata) {
+  const metadataContainer = d3.select("#sample-metadata");
+  metadataContainer.html("");
+
+  Object.entries(selectedMetadata).forEach(([key, value]) => {
+    metadataContainer
+      .append("p")
+      .text(`${key}: ${value}`);
+  });
+}
+
+// Function to update the bar chart
+function updateBarChart(selectedIndividual) {
   Plotly.update('bar', {
     x: [selectedIndividual.sample_values.slice(0, 10).reverse()],
     y: [selectedIndividual.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse()],
     text: [selectedIndividual.otu_labels.slice(0, 10).reverse()]
   });
+}
 
-  // Update the bubble chart data
+// Function to update the bubble chart
+function updateBubbleChart(selectedIndividual) {
   Plotly.update('bubble', {
-      x: [selectedIndividual.otu_ids],
-      y: [selectedIndividual.sample_values],
-      text: [selectedIndividual.otu_labels],
-      'marker.size': [selectedIndividual.sample_values],
-      'marker.color': [selectedIndividual.otu_ids],
+    x: [selectedIndividual.otu_ids],
+    y: [selectedIndividual.sample_values],
+    text: [selectedIndividual.otu_labels],
+    'marker.size': [selectedIndividual.sample_values],
+    'marker.color': [selectedIndividual.otu_ids],
   });
-});
+}
 
 // Call the loadJSON function to load the data
 loadJSON();
-
-
